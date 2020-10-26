f81b0e0e6bf4a2fb14a969415f5ae7838f795c74
Sano206 <gajdos.alexander206@gmail.com>
2020-10-26 13:59:07 +0100

    First commit

---
+++ src/main/kotlin/bakalarka/elearningplatform/ElearningplatformApplication.kt
@@ -0,0 +1,11 @@
+package bakalarka.elearningplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElearningplatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElearningplatformApplication>(*args)
+}
