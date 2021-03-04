29dcc738ae750136465e7e8a1446b3417e10f93c
Sano206 <gajdos.alexander206@gmail.com>
2021-03-04 15:20:19 +0100

    working login

---
+++ src/main/kotlin/bakalarka/elearningplatform/security/SecurityConfig.kt
@@ -0,0 +1,65 @@
+package bakalarka.elearningplatform.security
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.jwt.JwtDecoders
+
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+
+import org.springframework.security.oauth2.jwt.JwtValidators
+
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.List
+
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    @Value("\${auth0.audience}")
+    private val audience: String? = null
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private val issuer: String? = null
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/**").permitAll() // GET requests don't need auth
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .decoder(jwtDecoder())
+    }
+
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedMethods = listOf(
+                HttpMethod.GET.name,
+                HttpMethod.PUT.name,
+                HttpMethod.POST.name,
+                HttpMethod.DELETE.name
+        )
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues())
+        return source
+    }
+
+    fun jwtDecoder(): JwtDecoder {
+        val withAudience: OAuth2TokenValidator<Jwt> = AudienceValidator(audience!!)
+        val withIssuer = JwtValidators.createDefaultWithIssuer(issuer)
+        val validator: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withAudience, withIssuer)
+        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer) as NimbusJwtDecoder
+        jwtDecoder.setJwtValidator(validator)
+        return jwtDecoder
+    }
+}
