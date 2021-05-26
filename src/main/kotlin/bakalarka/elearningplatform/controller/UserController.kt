35c87f6feb32dea989279da60b126de122ef1dfc
Sano206 <gajdos.alexander206@gmail.com>
2021-05-26 19:13:20 +0200

    code cleanup

--- src/main/kotlin/bakalarka/elearningplatform/controller/UserController.kt
+++ src/main/kotlin/bakalarka/elearningplatform/controller/UserController.kt
@@ -12,25 +12,7 @@ class UserController(
     var enrollmentService: EnrollmentService,
     var userService: UserService
 ) {
-
-/*    @GetMapping("")
-    fun getAll() = userService.getAll()*/
-
     @PutMapping("")
     @PreAuthorize("true")
     fun update(@RequestBody request: UpdateUserRequest) = userService.update(request)
-
-/*    @GetMapping("/{userId}")
-    fun get(@PathVariable userId: Long) = userService.get(userId)
-
-    @PutMapping("/{userId}")
-    fun update(
-            @PathVariable userId: Long,
-            @RequestBody request: AddUserRequest) = userService.update(request, userId)
-
-    @GetMapping("/{userId}/enrollments") //
-    fun findByUserId(@PathVariable userId: Long) = enrollmentService.findByUserId(userId)
-
-    @DeleteMapping("/{userId}")
-    fun delete(@PathVariable userId: Long) = userService.delete(userId)*/
 }
